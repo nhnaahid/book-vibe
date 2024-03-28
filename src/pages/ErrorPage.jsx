57b7bf623cf29e55de02c08764f7de15d4cfdd80
nhnaahid <nhnahid.iu@gmail.com>
2024-03-28 20:46:36 +0600

    errorpage update

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,4 +1,4 @@
-import { Link, useRouteError } from "react-router-dom";
+// import { Link, useRouteError } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
